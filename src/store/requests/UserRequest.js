bbd16c3dae3af530c8ca48b7290a3085b810364f
MsYarusa <zaporozhskii.yl@dvfu.ru>
2024-01-09 19:06:38 +1000

    Попытка настроить CD №4 Добавил расширения в импорт файлов в файле App

--- src/store/requests/UserRequest.js
+++ src/store/requests/UserRequest.js
@@ -10,64 +10,64 @@ export const getUser = createAsyncThunk(
     let data = null;
 
     // для тестирования на localhost
-    try {
-      if (login === "abobus" && password === "abobus") {
-        data = {
-          token: "token",
-          first_name: "Abobus",
-          last_name: "banfuciy",
-          role: "manager",
-        };
-      } else if (login === "russianBear" && password === "123") {
-        data = {
-          token: "token",
-          first_name: "Vladimir",
-          last_name: "Putin",
-          role: "admin",
-        };
-      } else if (login === "galichka" && password === "123") {
-        data = {
-          token: "token",
-          first_name: "Galina",
-          last_name: "Otmena",
-          role: "cashier",
-        };
-      } else if (login === "russianBear2" && password === "123") {
-        data = {
-          token: "token",
-          first_name: "Dmitry",
-          last_name: "Medvedev",
-          role: "director",
-        };
-      } else {
-        throw new Error("Wrong login or password!");
-      }
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-
-    // отправление запроса
     // try {
-    //   await axios
-    //     .post("https://api.spacekot.ru/apishechka/login", {
-    //       login: CryptoJS.SHA256(login).toString(CryptoJS.enc.Hex),
-    //       password: CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex),
-    //     })
-    //     .then((res) => {
-    //       console.log("статус: успешно");
-    //       console.log("данные: ", res.data);
-    //       data = res.data;
-    //       if (data.role === "driver") {
-    //         throw new Error("Wrong login or password!");
-    //       }
-    //     })
-    //     .catch((error) => {
-    //       Sentry.captureException(error);
-    //       throw new Error(error.message);
-    //     });
+    //   if (login === "abobus" && password === "abobus") {
+    //     data = {
+    //       token: "token",
+    //       first_name: "Abobus",
+    //       last_name: "banfuciy",
+    //       role: "manager",
+    //     };
+    //   } else if (login === "russianBear" && password === "123") {
+    //     data = {
+    //       token: "token",
+    //       first_name: "Vladimir",
+    //       last_name: "Putin",
+    //       role: "admin",
+    //     };
+    //   } else if (login === "galichka" && password === "123") {
+    //     data = {
+    //       token: "token",
+    //       first_name: "Galina",
+    //       last_name: "Otmena",
+    //       role: "cashier",
+    //     };
+    //   } else if (login === "russianBear2" && password === "123") {
+    //     data = {
+    //       token: "token",
+    //       first_name: "Dmitry",
+    //       last_name: "Medvedev",
+    //       role: "director",
+    //     };
+    //   } else {
+    //     throw new Error("Wrong login or password!");
+    //   }
     // } catch (error) {
     //   return rejectWithValue(error.message);
     // }
+
+    // отправление запроса
+    try {
+      await axios
+        .post("https://api.spacekot.ru/apishechka/login", {
+          login: CryptoJS.SHA256(login).toString(CryptoJS.enc.Hex),
+          password: CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex),
+        })
+        .then((res) => {
+          console.log("статус: успешно");
+          console.log("данные: ", res.data);
+          data = res.data;
+          if (data.role === "driver") {
+            throw new Error("Wrong login or password!");
+          }
+        })
+        .catch((error) => {
+          Sentry.captureException(error);
+          throw new Error(error.message);
+        });
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
     return data;
   }
 );
